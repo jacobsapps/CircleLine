0e8dd2b4d2f4e7a8b7d4de528efe66c28dabba60
Jacob Bartlett <jacob@lapse.com>
2025-03-18 16:00:02 +0000

    Initial Commit

---
+++ MapIsGood/MapIsGoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapIsGoodApp.swift
+//  MapIsGood
+//
+//  Created by Jacob Bartlett on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MapIsGoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
